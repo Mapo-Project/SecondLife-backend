80bded66358742c3454c105438021da5150c7319
Kim-Daehee123 <rkqkdxma@naver.com>
2022-07-01 17:53:58 +0900

    user api 1차 완료

--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -1,9 +1,13 @@
 export class UserDto {
 <<<<<<< HEAD
+<<<<<<< HEAD
   social_id: string;
 =======
   user_id: string;
 >>>>>>> abb0d7b (auth api 수정)
+=======
+  social_id: string;
+>>>>>>> be0733b (user api 1차 완료)
   method: string;
   email: string;
   profile_img: string;
